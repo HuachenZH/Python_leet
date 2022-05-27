e54186c88ed0f5a3d0de8a2cad804ca357bbc07e
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-05-27 14:34:40 +0200

    built solution 2

--- Math/1512 Number of good pairs/1512.py
+++ Math/1512 Number of good pairs/1512.py
@@ -1,3 +1,4 @@
+# solution 1: with a list
 class Solution(object):
     def numIdenticalPairs(self, nums):
         """
@@ -19,3 +20,24 @@ class Solution(object):
         return len(outList)
 sol=Solution()
 sol.numIdenticalPairs([1,1,1,1])
+
+
+
+# solution 2 : no list, just count
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+        count=0;
+        # first for loop : i
+        for i in range(len(nums)-1):
+            # second for loop : j
+            for j in range(i+1,len(nums)):
+                # now we already fulfil the condition i<j
+                # let's check if nums[i] == nums[j] 
+                if nums[i] == nums[j]:
+                    count=count+1
+        return count
