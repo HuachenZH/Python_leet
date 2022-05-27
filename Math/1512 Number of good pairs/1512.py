00a81307e572f7bdf0910a98104b1cbd9371b8fc
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-05-27 14:13:09 +0200

    construct for loops
    error with the second for loop. In the output, there are pairs like :
    3 1
    3 2
    which are not correct

--- Math/1512 Number of good pairs/1512.py
+++ Math/1512 Number of good pairs/1512.py
@@ -1,1 +1,16 @@
-
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+        
+        # first for loop : i
+        for i in range(len(nums)-1):
+            # second for loop : j
+            for j in range(1,len(nums)):
+                print(i,j)
+        return "finish"
+sol=Solution()
+sol.numIdenticalPairs([1,2,3,1,1,3])
