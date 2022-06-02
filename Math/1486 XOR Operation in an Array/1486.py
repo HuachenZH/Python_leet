dcb820b47d7ebe0183b43ee6e813ef8f197d20dd
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-06-02 09:35:15 +0200

    Update 1486.py

--- Math/1486 XOR Operation in an Array/1486.py
+++ Math/1486 XOR Operation in an Array/1486.py
@@ -2,9 +2,13 @@ class Solution:
     def xorOperation(self, n: int, start: int) -> int:
         nums=[]
         for i in range(n):
-            nums.append(2*i)
-        res=0
-        for i in nums:
-        	res=res^i
-
+            nums.append(start+2*i)
+        res=nums[0]
+        print(nums)
+        for i in range(1,len(nums)):
+        	res=res^nums[i]
+        	print(res)
         return res
+
+sol=Solution()
+print(sol.xorOperation(4,3))
