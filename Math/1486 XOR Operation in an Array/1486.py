23a00a0a6f7cad9e261b10075a9b39f842b1c330
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-06-02 09:29:49 +0200

    finished

--- Math/1486 XOR Operation in an Array/1486.py
+++ Math/1486 XOR Operation in an Array/1486.py
@@ -1,1 +1,10 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        nums=[]
+        for i in range(n):
+            nums.append(2*i)
+        res=0
+        for i in nums:
+        	res=res^i
 
+        return res
