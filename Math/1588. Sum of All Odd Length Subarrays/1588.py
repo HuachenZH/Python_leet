73ad4efb78a0bfdde974c825b52dcb54b595eefc
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-06-07 23:48:24 +0200

    Create 1588.py

---
+++ Math/1588. Sum of All Odd Length Subarrays/1588.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sumOddLengthSubarrays(self, arr: list[int]) -> int:
+        # a list of odd numbers from 0 to 100... wait it's odd number but not prime number
+        oddList=list(range(1,len(arr)+1,2))
+        # make subarrays
+        # sum
+        
+        
+        
+        return oddList
+
+sol=Solution()
+print(sol.sumOddLengthSubarrays([1,4,2,5,3]))
