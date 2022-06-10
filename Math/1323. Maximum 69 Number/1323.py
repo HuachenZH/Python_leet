3d82348a495f9e549612bb4d43b6de32cb6c26c0
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-06-10 23:23:07 +0200

    finished

---
+++ Math/1323. Maximum 69 Number/1323.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        for i in range(len(str(num))-1,-1,-1): # decrementing with range.
+            if int(num/10**i%10)==6:
+                num+=3*10**i
+                break
+        return num
+            
+
+
+
+
+sol=Solution()
+print(sol.maximum69Number(9669))
