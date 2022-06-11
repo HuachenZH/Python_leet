9d83f6f91e753249cfc2f47f13d6ab522070e9ce
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-06-11 15:42:56 +0200

    Update 1323.py

--- Math/1323. Maximum 69 Number/1323.py
+++ Math/1323. Maximum 69 Number/1323.py
@@ -1,5 +1,6 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
+        # a pure maths approach
         for i in range(len(str(num))-1,-1,-1): # decrementing with range.
             if int(num/10**i%10)==6:
                 num+=3*10**i
