564fbc104ec40f9d28517a41af57ec21b9a02891
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-06-15 23:39:41 +0200

    Update 1281.py

--- Math/1281 Subtract the Product and Sum of Digits of an Integer/1281.py
+++ Math/1281 Subtract the Product and Sum of Digits of an Integer/1281.py
@@ -16,3 +16,21 @@ class Solution:
         return result
 sol=Solution()
 sol.subtractProductAndSum(125)
+
+
+class Solution2:
+    def getDigits(self,n:int) -> list:
+        '''Get all the digits of a number and save them into a list.
+        Pure Math approach.
+        The return list will be [ones digit, tens digit, hundreds digti...] '''
+        table=[]
+        # get the one digit
+        table.append(n%10)
+        # get the other digits
+        for i in range(len(str(n))-1):
+            n=(n-table[len(table)-1])/10
+            print(n)
+            table.append(int(n%10))
+        return table
+sol2=Solution2()
+print(sol2.getDigits(1234))
