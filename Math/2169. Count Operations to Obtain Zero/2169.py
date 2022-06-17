dfa7603a0ed92ef9cee5cd42291d9fa922b739d2
HuachenZH <huachen.zhang.23@eigsi.fr>
2022-06-17 23:09:11 +0200

    finished

---
+++ Math/2169. Count Operations to Obtain Zero/2169.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        if num1==0 and num2==0:
+            return 0
+        count=0
+        while num1!=0 and num2!=0:
+            if num1>=num2:
+                num1-=num2
+            else:
+                num2-=num1
+            count+=1
+        return count
+sol=Solution()
+print(sol.countOperations(0,0))
