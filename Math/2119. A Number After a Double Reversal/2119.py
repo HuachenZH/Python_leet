8224e2789792395e33ab8dcc9bc53b97d3ecc467
HuachenZH <huachen.zhang.23@eigsi.fr>
2022-06-17 22:43:58 +0200

    add programmer approach

--- Math/2119. A Number After a Double Reversal/2119.py
+++ Math/2119. A Number After a Double Reversal/2119.py
@@ -6,3 +6,12 @@ class Solution:
         return num%10!=0
 
 # Programmer:
+class Solution2:
+    def isSameAfterReversals(self, num: int) -> bool:
+        # the string approach will be faster... i suppose
+        return int(str(int(str(num)[::-1]))[::-1])==num
+        #              |------------------|          --> first reverse
+        #      |------|                    |-----|   --> second reverse
+sol=Solution2()
+print(sol.isSameAfterReversals(1200))
+
