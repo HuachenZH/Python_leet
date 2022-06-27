50946f9daa675544a783a80ad2a3ca0a3f5b3a72
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-06-27 20:33:43 +0200

    Delete Math/908. Smallest Range I directory

--- Math/908. Smallest Range I/908.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def smallestRangeI(self, nums: List[int], k: int) -> int:
-        nums=np.array(nums)
-		
-        return np.average(nums)
-sol=Solution()
-print(sol.smallestRangeI([1,10,5,40],20))
