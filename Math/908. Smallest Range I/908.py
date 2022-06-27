83bcebf6cf1be3eca9868d496098445d804442c2
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-06-27 17:59:14 +0200

    Update 908.py

--- Math/908. Smallest Range I/908.py
+++ Math/908. Smallest Range I/908.py
@@ -1,3 +1,7 @@
 class Solution:
     def smallestRangeI(self, nums: List[int], k: int) -> int:
-        
+        nums=np.array(nums)
+		
+        return np.average(nums)
+sol=Solution()
+print(sol.smallestRangeI([1,10,5,40],20))
