ac4ad2b7546a6d66f39417d4e3e84df386264ce3
HuachenZH <huachen.zhang.23@eigsi.fr>
2022-07-25 21:01:47 +0200

    test passed

--- Math/258. Add Digits/258.py
+++ Math/258. Add Digits/258.py
@@ -1,3 +1,26 @@
 class Solution:
-    def addDigits(self, num: int) -> int:
-        
+    # Cf 2180 digitSum
+    def digitSum(self,n:int) -> int:
+        table=[]
+        # get the one digit
+        table.append(n%10)
+        # get the other digits
+        for i in range(len(str(n))-1):
+            n=(n-table[len(table)-1])/10
+            table.append(int(n%10))
+        return sum(table)
+    # solution: recursive
+    def addDigitsr(self, num: int) -> int:
+        if len(str(num))>1:
+            return self.addDigits(self.digitSum(num))
+        else:
+            return num
+    # solution: iterative
+    def addDigitsi(self, num: int) -> int:
+        while int(num/10)>0: # more than one digit
+            num=self.digitSum(num)
+        return num
+epsilon=Solution()
+print(epsilon.addDigitsr(38))
+print(epsilon.addDigitsi(38))
+
