e6acef9db15c291695fb57dd79e11792e654d4cc
HuachenZH <huachen.zhang.23@eigsi.fr>
2022-08-02 00:04:59 +0200

    solution updated

--- Algo/1. Two Sum/1.py
+++ Algo/1. Two Sum/1.py
@@ -5,5 +5,17 @@ class Solution:
                 if nums[i]+nums[j]==target:
                     return [i,j]
         return '404'
+    
+    # solution of leet: one-pass hashmap
+    # add elements of the list nums into a dictionary as key, their index as value
+    # while adding, check if the complementry already exists in the dictionary
+    def twoSum2(self, nums: list[int], target: int) -> list[int]:
+        dictionary={}
+        for i in range(len(nums)):
+            if target-nums[i] in dictionary:
+                return [i, dictionary[target-nums[i]]]
+            dictionary[nums[i]]=i
+    # we add nums[i] into the dictionary after check if, to avoid the situation that nums[i]+nums[i]==target
+
 epsilon=Solution()
-print(epsilon.twoSum([2,7,11,15],9))
+print(epsilon.twoSum2([2,7,11,15],9))
