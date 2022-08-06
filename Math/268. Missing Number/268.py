e2646db9e481e11aca58c720858bc55956526a21
HuachenZH <huachen.zhang.23@eigsi.fr>
2022-08-06 10:03:13 +0200

    test passed

--- Math/268. Missing Number/268.py
+++ Math/268. Missing Number/268.py
@@ -1,3 +1,12 @@
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        
+    def termial(self,num:int) -> int:
+        res=0
+        for i in range(num+1):
+            res+=i
+        return res
+    def missingNumber(self, nums: list[int]) -> int:
+        return self.termial(len(nums))-sum(nums)
+    
+epsilon=Solution()
+print(epsilon.missingNumber([9,6,4,2,3,5,7,0,1]))
+
