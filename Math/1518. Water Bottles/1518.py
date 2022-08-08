3e91e5c2220b8eb03ae1a72c54998e0bdf6d84c7
HuachenZH <huachen.zhang.23@eigsi.fr>
2022-08-08 20:06:09 +0200

    test passed

--- Math/1518. Water Bottles/1518.py
+++ Math/1518. Water Bottles/1518.py
@@ -1,3 +1,11 @@
+import math
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
-        
+        res=numBottles
+        emptyBottles=numBottles
+        while emptyBottles>=numExchange:
+            res+=math.floor(emptyBottles/numExchange)
+            emptyBottles=math.floor(emptyBottles/numExchange)+emptyBottles%numExchange
+        return res
+epsilon=Solution()
+print(epsilon.numWaterBottles(9,3))
