04e5939c0c3f3399573404e01486338fab16505f
HuachenZH <huachen.zhang.23@eigsi.fr>
2022-08-14 18:54:00 +0200

    test passed

--- Math/1903. Largest Odd Number in String/1903.py
+++ Math/1903. Largest Odd Number in String/1903.py
@@ -1,3 +1,11 @@
 class Solution:
     def largestOddNumber(self, num: str) -> str:
-        
+        if int(num)%2!=0: return num
+        else: # int(num) is even
+            for i in range(len(num)-1,-1,-1):
+                if int(num[i])%2!=0: # the digit is odd
+                    return num[:i+1]
+            return ''
+
+epsilon=Solution()
+print(epsilon.largestOddNumber("4206"))
