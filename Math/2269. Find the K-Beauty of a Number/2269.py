4ceee6d7a22feb9b3a589aae81ff7a1a89f34a94
HuachenZH <huachen.zhang.23@eigsi.fr>
2022-08-14 18:13:47 +0200

    test passed

--- Math/2269. Find the K-Beauty of a Number/2269.py
+++ Math/2269. Find the K-Beauty of a Number/2269.py
@@ -1,3 +1,14 @@
 class Solution:
     def divisorSubstrings(self, num: int, k: int) -> int:
-        
+        res=0
+        tmp=0
+        for i in range(len(str(num))-k+1):
+            tmp=int(str(num)[i:i+k])
+            if tmp==0:
+                continue
+            else:
+                if num%tmp==0:
+                    res+=1
+        return res
+epsilon=Solution()
+print(epsilon.divisorSubstrings(430043,2))
