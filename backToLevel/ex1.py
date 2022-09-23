4441fcb027ccda14cb6e064b2b47026a4dea2606
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-09-23 10:13:07 +0200

    Update ex1.py

--- backToLevel/ex1.py
+++ backToLevel/ex1.py
@@ -23,34 +23,30 @@ class Solution:
     # donner un score de vraisemblance a partir de la frequence des lettres
     def value(self, key:int, texte: str) -> dict:
         outdict={}
-
         newText=self.shift(key,texte)
         for i in range(97,123):
-            #
             outdict[chr(i)]=newText.count(chr(i))/len(texte)*100
         return outdict
 
     # donne le decodage le plus vraisemblable vis a vis de la frequence des lettres
-    
     def decode_cesar(self, texte:str) -> int:
         #
         valueDict={}
         scorelist=[]
-        # sorted by a to z
+        # sorted by a to z. Made in Excel
         vectStandard=numpy.array([7.68,0.80,3.32,3.60,17.76,1.06,1.10,0.64,7.23,0.19,0.00,5.89,2.72,7.61,5.34,3.24,1.34,6.81,8.23,7.30,6.05,1.27,0.0,0.54,0.21,0.07])
         for i in range(1,26):
-            valueDict=self.value(i, texte)
-            vectThis=[]
-            for key in sorted(valueDict):
-                # print (key, valueDict[key])
-                vectThis.append(valueDict[key]) # score of each letter, sorted by a to z
-            vectThis=numpy.array(vectThis)
-            score=(numpy.sum((vectStandard[:]-vectThis[:])**2))**0.5
+            valueDict=self.value(i, texte) # score of a decoding
+            vectScoped=list([valueDict[key] for key in sorted(valueDict)])
+            vectScoped=numpy.array(vectScoped)
+            score=(numpy.sum((vectStandard[:]-vectScoped[:])**2))**0.5
             scorelist.append(score)
             # print(score)
         print('min score is : '+str(min(scorelist)))
         scorelist=numpy.array(scorelist)
-        return numpy.where(scorelist == scorelist.min())[0][0]+1
+        print('decoded messaged :')
+        print('value of shift : '+ str(numpy.where(scorelist == scorelist.min())[0][0]+1))
+        return self.shift(numpy.where(scorelist == scorelist.min())[0][0]+1,texte)
 
 
 epsilon=Solution()
