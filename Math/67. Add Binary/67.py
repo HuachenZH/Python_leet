735ae6672d4aabe42521da8ec2b746d652852330
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-10-13 15:34:30 +0200

    Update 67.py

--- Math/67. Add Binary/67.py
+++ Math/67. Add Binary/67.py
@@ -3,6 +3,8 @@ class Solution:
         res = str(bin(int(a,2)+int(b,2)))[2:]
         return res
 
+# The int() function takes as second argument the base of the number to be converted, which is 2 in case of binary numbers.
+    
 if __name__ == "__main__":
     epsilon = Solution()
     print(epsilon.addBinary("1","1"))
