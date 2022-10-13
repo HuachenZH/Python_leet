65121c46280480ed99c02b5e0c682d9255a80356
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-10-13 15:32:55 +0200

    Update 67.py

--- Math/67. Add Binary/67.py
+++ Math/67. Add Binary/67.py
@@ -1,3 +1,8 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        
+        res = str(bin(int(a,2)+int(b,2)))[2:]
+        return res
+
+if __name__ == "__main__":
+    epsilon = Solution()
+    print(epsilon.addBinary("1","1"))
