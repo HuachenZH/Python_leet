de43e9e856cb7fef2ad07a1fbbc4e420d7170946
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-10-13 11:58:41 +0200

    Update 70.py

--- Math/70. Climbing Stairs/70.py
+++ Math/70. Climbing Stairs/70.py
@@ -1,3 +1,23 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
+        # 1 step : 1 way
+        # 2 step : 2 way
+        # 3 step : 2+1=3 way
+        # 4 step : 2+3=5 way
+        # s(n) = s(n-1) + s(n-2)
+        if n==1:
+        	return 1
+        if n==2:
+        	return 2
+        minus1=1
+        minus2=2
+        for i in range(n-2):
+        	res = minus1 + minus2
+        	minus1 = minus2
+        	minus2 = res
+        return res
+
+if __name__ == "__main__":
+    epsilon = Solution()
+    print(epsilon.climbStairs(5))
+
