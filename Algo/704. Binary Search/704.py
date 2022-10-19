bbad163b39009b71fd93185348756a98a6e28de2
unknown <huachen.zhang.23@eigsi.fr>
2022-10-19 15:51:24 +0200

    nothing built

--- Algo/704. Binary Search/704.py
+++ Algo/704. Binary Search/704.py
@@ -1,3 +1,13 @@
+import math
+
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        
+        left = 0
+        right = len(nums)
+        print(nums[len(nums)-1])
+        return 
+
+
+if __name__ == '__main__':
+    epsilon = Solution()
+    print(epsilon.search([-1,0,3,5,9,12], 9))
