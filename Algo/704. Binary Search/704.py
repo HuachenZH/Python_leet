738efa7a3d5445f722001ac65590ee1dedf5bef7
HuachenZH <huachen.zhang.23@eigsi.fr>
2022-10-19 20:18:53 +0200

    pb at stopping while

--- Algo/704. Binary Search/704.py
+++ Algo/704. Binary Search/704.py
@@ -2,12 +2,32 @@ import math
 
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        left = 0
-        right = len(nums)
-        print(nums[len(nums)-1])
-        return 
+        leftIndex = 0
+        rightIndex = len(nums)-1
+        # firstly use flag, then replace it by the true condition
+        flag = True
+        counter = 0
+        while leftIndex < rightIndex :
+            middleIndex = math.floor((leftIndex+rightIndex)/2)
+            print("middle index is %s, gives %s" % (middleIndex, nums[middleIndex]))
+            if target == nums[middleIndex]:
+                res = middleIndex
+            elif target < nums[middleIndex] :
+                rightIndex = middleIndex - 1
+                print("comparing target %s with %s" % (target, nums[middleIndex]), end=' , ')
+                print('take the left interval')
+            else:
+                leftIndex = middleIndex + 1
+                print("comparing target %s with %s" % (target, nums[middleIndex]), end=' , ')
+                print('take the right interval')
+            print("%s iteration ends, left index gives %s, right index gives %s" % (counter, nums[leftIndex], nums[rightIndex]))
+            print("left index is %s, right index is %s" % (leftIndex, rightIndex))
+            print('')
+            counter += 1
+
+        return res
 
 
 if __name__ == '__main__':
     epsilon = Solution()
-    print(epsilon.search([-1,0,3,5,9,12], 9))
+    print(epsilon.search([-1,0,3,5,9,12,13], 12))
