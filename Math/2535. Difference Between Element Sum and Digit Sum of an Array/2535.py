33e9a860fdb8b957a1c8df3a6c110874d1be7a86
HuachenZH <huachen.zhang.23@eigsi.fr>
2023-02-05 14:50:51 +0100

    add all

--- Math/2535. Difference Between Element Sum and Digit Sum of an Array/2535.py
+++ Math/2535. Difference Between Element Sum and Digit Sum of an Array/2535.py
@@ -1,2 +1,18 @@
 class Solution:
-    def differenceOfSum(self, nums: List[int]) -> int:
+    # ver alpha
+    def differenceOfSum(self, nums: list[int]) -> int:
+        elementSum = sum(nums)
+        numsStr = [] # create an empty list, where digit sum of each number of nums  will be stored
+        for num in nums:
+            numsStr.append(sum(int(digit) for digit in [*str(num)]))
+            #                                          ^^^^^^^^^^  = split every single character of the string and there will be a list
+        print("numsStr is %s" % numsStr)
+        digitSum = sum(numsStr)
+        return abs(digitSum - elementSum)
+epsi = Solution()
+print(epsi.differenceOfSum([1,15,6,3]))
+
+# to split every single character in a string, to methods to do so:
+# word = "word"
+# list(word)
+# [*word]
