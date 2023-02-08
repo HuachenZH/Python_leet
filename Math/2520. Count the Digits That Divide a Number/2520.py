18d4fdceeebe4850f6f154f5d374dc7e944271a3
HuachenZH <huachen.zhang.23@eigsi.fr>
2023-02-08 20:40:15 +0100

    add all
    i m so low somebody save me

--- Math/2520. Count the Digits That Divide a Number/2520.py
+++ Math/2520. Count the Digits That Divide a Number/2520.py
@@ -1,2 +1,19 @@
 class Solution:
     def countDigits(self, num: int) -> int:
+        # get every digit of the number, store them into a list then delete duplicates
+
+        # int to str, split, str to int
+        numList = list(int(key) for key in [*str(num)])
+        #                                                 ^^^^^^^^^^ int to str then split each character
+        #              ^^^^^^^^^^^^^^^^^^                            str to int
+        print(numList)
+        
+        # check num can be divided by which digit
+        res = 0
+        for digit in numList:
+            if num % digit ==0:
+                res -=- 1
+        return res
+
+epsi = Solution()
+print(epsi.countDigits(121))
