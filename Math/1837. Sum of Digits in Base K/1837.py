4a9e8916ec1df2a966a58e57825b5ffc2958c0c0
HuachenZH <huachen.zhang.23@eigsi.fr>
2023-02-10 19:58:11 +0100

    test passed
    how to escape the reality, im so low. Save me

--- Math/1837. Sum of Digits in Base K/1837.py
+++ Math/1837. Sum of Digits in Base K/1837.py
@@ -1,3 +1,10 @@
 class Solution:
     def sumBase(self, n: int, k: int) -> int:
-        
+        listres = []
+        while n > 0:
+            listres.append(n%k)
+            n = n //k
+        print(listres)
+        return sum(listres)
+epsi = Solution()
+print(epsi.sumBase(34,6))
