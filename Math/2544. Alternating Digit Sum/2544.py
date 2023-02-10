69299d598618a4f145270d8b817181be9b5d86ee
HuachenZH <huachen.zhang.23@eigsi.fr>
2023-02-10 20:18:00 +0100

    test passed
    it s not true this is not real life i ve been in a nightmare why everything so down and sad

--- Math/2544. Alternating Digit Sum/2544.py
+++ Math/2544. Alternating Digit Sum/2544.py
@@ -1,2 +1,21 @@
+import numpy as np
 class Solution:
     def alternateDigitSum(self, n: int) -> int:
+        # try to solve it in a matlab style
+        # the performance (runtime, memory) is not optimized though...
+
+        # mapping vector
+        tmp = []
+        for i in range(len(str(n))):
+            tmp.append((-1)**i)
+        mapping = np.array(tmp)
+        print(mapping)
+        # input vector
+        tmp = []
+        for digit in [*str(n)]:
+            tmp.append(int(digit))
+        invect = np.array(tmp)
+        # scalar product and sum up
+        return np.dot(invect, mapping).sum()
+epsi = Solution()
+print(epsi.alternateDigitSum(111))
