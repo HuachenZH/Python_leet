3490d974fb25d9c6cd89f4cadd7c621f1ce90886
HuachenZH <huachen.zhang.23@eigsi.fr>
2023-02-14 21:24:42 +0100

    test passed
    hard time s arriving. I m not ready. why must be strong why cannot be weak

--- Math/1103. Distribute Candies to People/1103.py
+++ Math/1103. Distribute Candies to People/1103.py
@@ -7,11 +7,26 @@ class Solution:
         candiesToGive = 1
         indexPeople = 0
         while remain >= candiesToGive:           
+            print("indexPeople is %s" % indexPeople)
+            print("candiesToGive is %s" % candiesToGive)
             ans[indexPeople] += candiesToGive
+            remain -= candiesToGive
             candiesToGive -=- 1
             indexPeople -=- 1
             if indexPeople > num_people-1:
                 indexPeople = 0
-            remain -= candiesToGive
+                print("indexPeople reset")
+            print("remain is %s" % remain)
+            print("answer is %s" % ans)
+            print('\n')
         ans[indexPeople] += remain
         return ans
+
+epsi = Solution()
+print(epsi.distributeCandies(7,4))
+
+# i think there might be pure maths approach.
+# after half an hour playing with equations, i abandonned pure maths approach
+# the algo is simple and stupid, however the performance is not bad
+# runtime 81.93%
+# memory 43.7%
