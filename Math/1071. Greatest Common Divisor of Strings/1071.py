38bceaa7dcb0890ad7a0c005528970a8e799938f
HuachenZH <huachen.zhang.23@eigsi.fr>
2023-02-26 16:05:54 +0100

    test passed
    finally i get a job. So what s next

--- Math/1071. Greatest Common Divisor of Strings/1071.py
+++ Math/1071. Greatest Common Divisor of Strings/1071.py
@@ -1,5 +1,7 @@
-import math
 class Solution:
+    # runtime could be better. 
+    # runtime 47.26%
+    # memory 63.35%
     def divisibleString(self, str1: str, str2: str) -> bool:
         '''
         Check if the str1 % str2 == 0. 
@@ -25,13 +27,37 @@ class Solution:
             compare += str2
         return compare == str1
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        return
+        # if their lengths are equal, then only need to test if str1 % str2 == 0
+        if len(str1) == len(str2):
+            if self.divisibleString(str1, str2):
+                return str1
+            return ""
+        # convention: str1 is the larger, str2 is the smaller
+        if len(str1)<len(str2):
+            tmp = str2
+            str2 = str1
+            str1 = tmp
+            del tmp
+        # new str1 is the larger, str2 is the smaller
+
+        # in order to be a gcd, it must be a factor of the smaller string, then a factor of the larger string
+        for i in range(len(str2)-1,-1,-1):
+            # test if str2[0:i] is a commun factor
+            # print(str2[0:i+1])
+            # print(i)
+            # firstly it must be a factor of the smaller stirng
+            if self.divisibleString(str2, str2[0:i+1]):
+                # check for the larger string
+                if self.divisibleString(str1, str2[0:i+1]):
+                    return str2[0:i+1]
+        return ""
 epsi = Solution()
 print(epsi.divisibleString("ABCABC", "ABC"))
 epsi.gcdOfStrings("ABCABC", "ABC")
 
-testCases = [("ABCABC", "ABC"), ("ABCABC", "AB")]
+testCases = [("ABCABC", "ABC"), ("ABCABC", "AB"), ("ABCDEF","ABC")]
 for testCase in testCases:
     print("Test case: %s %s" % (testCase[0], testCase[1]))
     print("- divisableString : %s" % epsi.divisibleString(testCase[0], testCase[1]))
+    print("- gcdOfStrings : %s" % epsi.gcdOfStrings(testCase[0], testCase[1]))
     print("")
