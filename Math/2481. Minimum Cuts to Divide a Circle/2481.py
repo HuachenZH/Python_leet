a4f7a2e1171e668d258e865f79dd113e33fe4f44
HuachenZH <huachen.zhang.23@eigsi.fr>
2023-03-02 15:45:45 +0100

    test passed

--- Math/2481. Minimum Cuts to Divide a Circle/2481.py
+++ Math/2481. Minimum Cuts to Divide a Circle/2481.py
@@ -1,2 +1,17 @@
 class Solution:
+    # i thought it would be much more complicate
+    # runtime 31 ms 70.62%
+    # memory 13.7 MB 91.88%
     def numberOfCuts(self, n: int) -> int:
+        # it makes me think of recursive.
+        # no wait it want equal slices
+        if n == 1:
+            return 0
+        if n % 2 == 0: # if n is even
+            return int(n/2)
+        else: # else, n is odd
+            return n
+        
+
+epsi = Solution()
+print(epsi.numberOfCuts(5))
