2a521e329f5a9950d8ffc5c279b842f055062b2b
HuachenZH <huachen.zhang.23@eigsi.fr>
2023-03-09 14:57:00 +0100

    test passed

--- Math/231. Power of Two/231.py
+++ Math/231. Power of Two/231.py
@@ -1,2 +1,17 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        
+        while n > 1:
+            n /= 2
+            if n % 1 != 0:
+                return False
+            # prepare for next iteration
+            # nothing to do for the next iteration
+            
+
+        return True
+
+epsi = Solution()
+print(epsi.isPowerOfTwo(64))
