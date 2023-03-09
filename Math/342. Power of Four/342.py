ef1516c6ad1e79c2f35741d41c3cc82a2e683031
HuachenZH <huachen.zhang.23@eigsi.fr>
2023-03-09 14:54:58 +0100

    342

--- Math/342. Power of Four/342.py
+++ Math/342. Power of Four/342.py
@@ -1,2 +1,24 @@
 class Solution:
+    # ver ??
+    # according to leetcode, there is solution without loops
+
+    # ver alpha
+    # surprisingly, the performance of this ver is not bad
+    # runtime 23 ms 98.76%
+    # memory 13.8 MB 91.56%
     def isPowerOfFour(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        
+        while n > 1:
+            n /= 4
+            if n % 1 != 0:
+                return False
+            # prepare for next iteration
+            # nothing to do for the next iteration
+            
+
+        return True
+
+epsi = Solution()
+print(epsi.isPowerOfFour(-64))
