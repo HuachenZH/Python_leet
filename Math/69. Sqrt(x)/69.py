a4a66f3655663cc3ec2ea73952f97da9ff3b2f0f
HuachenZH <huachen.zhang.23@eigsi.fr>
2023-03-27 21:22:23 +0200

    test passed
    first day  what will it be like

--- Math/69. Sqrt(x)/69.py
+++ Math/69. Sqrt(x)/69.py
@@ -1,3 +1,15 @@
 class Solution:
+    # Runtime 1563 ms 22.25%
+    # memory 13.9 MB 44.67%
     def mySqrt(self, x: int) -> int:
-        return int(x**0.49)
+        if x == 0:
+            return 0
+        if x == 1:
+            return 1
+        for i in range(1,x+1):
+            if i*i > x:
+                return i-1
+        return -1
+
+epsi = Solution()
+print(epsi.mySqrt(24))
