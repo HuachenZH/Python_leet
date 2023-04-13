d531928f7e5799324ddfecabcdf0e1e382fc9091
HuachenZH <huachen.zhang.23@eigsi.fr>
2023-04-13 21:20:53 +0200

    test passed

--- Math/2442. Count Number of Distinct Integers After Reverse Operations/2442.py
+++ Math/2442. Count Number of Distinct Integers After Reverse Operations/2442.py
@@ -1,2 +1,23 @@
 class Solution:
-    def countDistinctIntegers(self, nums: List[int]) -> int:
+
+    def reverseInt(self, num: int) -> int:
+        # reverse the integer by using string approach
+        return int(str(num)[::-1])
+
+    # runtime 799 ms 32.63%
+    # memory 39.7 MB 58.39%
+    # i'm not sure which approach will be faster for the reverseInt function, string approach or pure numeric approach.
+    def countDistinctIntegersBeta(self, nums: list[int]) -> int:
+        # the simplest approach, just do what it asks
+
+        # iterate through the original list, reverse the integer then append to the end
+        for i in range(len(nums)):
+            nums.append(self.reverseInt(nums[i]))
+        # delete duplicates in the list by using set
+        nums_distinct = list({num for num in nums})
+        return len(nums_distinct)
+    
+epsi = Solution()
+print(epsi.reverseInt(256))
+print(epsi.countDistinctIntegers([1,13,10,12,31]))
+print(epsi.countDistinctIntegers([2,2,2]))
