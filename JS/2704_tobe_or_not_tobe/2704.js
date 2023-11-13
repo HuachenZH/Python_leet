c0ad7762c73e9b2828ae06ce65f594ca1ffdbb3f
HuachenZH <huachen.zhang.23@eigsi.fr>
2023-11-13 14:04:16 +0100

    building

--- JS/2704_tobe_or_not_tobe/2704.js
+++ JS/2704_tobe_or_not_tobe/2704.js
@@ -2,12 +2,32 @@
  * @param {string} val
  * @return {Object}
  */
+/*
 var expect = function(val) {
-    
+    return {
+        toBe: function(val2) {
+            console.log(var2)
+        }
+    };
 };
     
 
-/**
- * expect(5).toBe(5); // true
- * expect(5).notToBe(5); // throws "Equal"
- */
+expect(5).toBe(5); // true
+// expect(5).notToBe(5); // throws "Equal"
+*/
+
+function expect(actualValue) {
+  return {
+    toBe: function(expectedValue) {
+      if (actualValue === expectedValue) {
+        console.log('Test passed!');
+      } else {
+        console.error(`Test failed! Expected ${expectedValue}, but got ${actualValue}`);
+      }
+    }
+  };
+}
+
+// Example usage:
+expect(5).toBe(5); // This should pass
+expect(10).toBe(5); // This should fail
