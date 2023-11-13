9f36cbf243102ca9155a18416062265b0f52d2bb
HuachenZH <huachen.zhang.23@eigsi.fr>
2023-11-13 13:45:31 +0100

    test passed

--- JS/2620_counter/2620.js
+++ JS/2620_counter/2620.js
@@ -2,16 +2,28 @@
  * @param {number} n
  * @return {Function} counter
  */
-var createCounter = function(n) {
-    
-    return function() {
-        
-    };
-};
 
-/** 
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */
+// first try, test passed
+var createCounter = function(n) {
+    var counter = n
+    // the line below printed only once, no matter how
+    // many times i called the counter
+    console.log("counter initialized")
+    return function() {
+        return counter++;
+    };
+}
+
+// second try, test passed
+var createCounter = function(n) {
+    return function(){
+        return n++;
+    }
+}
+
+
+
+const counter = createCounter(10)
+console.log(counter()) // 10
+console.log(counter()) // 11
+console.log(counter()) // 12
