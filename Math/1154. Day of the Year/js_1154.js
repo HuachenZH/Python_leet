8d9c5ed005223ff599e0cc04fcfc6c1193cab16a
HuachenZH <huachen.zhang.23@eigsi.fr>
2023-11-30 21:37:25 +0100

    js test passed

--- Math/1154. Day of the Year/js_1154.js
+++ Math/1154. Day of the Year/js_1154.js
@@ -3,8 +3,27 @@
  * @return {number}
  */
 var dayOfYear = function(date) {
-   return 
+    // Split to get day month and year
+    var year = Number(date.split("-")[0]);
+    var month = Number(date.split("-")[1]);
+    var day = Number(date.split("-")[2]);
+
+    // Check if it's a leap year or not.
+    // Notice that 1900 is not a leap year.
+    let mapp = [];
+    if(year % 4 == 0 && year != 1900) {
+        // Number of day of each month
+        mapp = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 0];
+    }else {
+        mapp = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 0];
+    }
+    // Accumulate the number of day until month -1
+    var initialValue = 0;
+    let res = mapp.slice(0, month-1).reduce(
+        ((accumulator, currentValue) => accumulator + currentValue), initialValue  );
+    res += day;
+    return res;
 };
 
 
-dayOfYear("2019-02-10")
+dayOfYear("2019-09-10")
