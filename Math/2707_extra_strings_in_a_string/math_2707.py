8bf7af701a245da921d76deee81413c13d8ff507
HuachenZH <huachen.zhang.23@eigsi.fr>
2024-09-23 21:43:11 +0200

    2707 needs debugging

--- Math/2707_extra_strings_in_a_string/math_2707.py
+++ Math/2707_extra_strings_in_a_string/math_2707.py
@@ -1,3 +1,14 @@
 class Solution:
-    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        
+    def minExtraChar_dep2(self, s: str, dictionary: list[str]) -> int:
+        res = len(s)
+        for word in dictionary:
+            if word in s:
+                res = res - len(word)
+        return res
+    
+
+
+    def minExtraChar_dep1(self, s: str, dictionary: list[str]) -> int:
+        for word in dictionary:
+            s = s.replace(word, "")
+        return len(s)
