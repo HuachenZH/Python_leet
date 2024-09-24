f9fbb34154db9e6ce546cce7565ea691c188e2ed
HuachenZH <huachen.zhang.23@eigsi.fr>
2024-09-24 13:25:33 +0200

    still trying 2707

--- Math/2707_extra_strings_in_a_string/assessment_2707.py
+++ Math/2707_extra_strings_in_a_string/assessment_2707.py
@@ -5,19 +5,36 @@ import unittest
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    # def test_upper_0(self):
+    #     solution = Solution()
+    #     self.assertEqual(solution.minExtraChar('dwmodizxvvbosxxw', ["ox","lb","diz","gu","v","ksv","o","nuq","r","txhe","e","wmo","cehy","tskz","ds","kzbu"]), 7)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_upper_1(self):
+        solution = Solution()
+        self.assertEqual(solution.minExtraChar('azvzulhlwxwobowijiyebeaskecvtjqwkmaqnvnaomaqnvf', ["na","i","edd","wobow","kecv","b","n","or","jj","zul","vk","yeb","qnfac","azv","grtjba","yswmjn","xowio","u","xi","pcmatm","maqnv"]), 15)
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+
+
+# Analysis
+# dwm o diz x v v bosxxw
+# ___ * *** _ * * ______
+# output: 8, incorrect
+# split at "o" so that i cannot split again at "wmo", and split at "wmo" is more optimized
+# and notice, in this case i need to split "v" two times
+# 
+# d wmo diz x v v bosxxw
+# _ *** *** _ * * ______  
+# output: 7, correct
+
+# imagine in "dictionary" you have "wmo" and "o"
+# after splitting "wmo", you cannot split the "o" inside again
+
+# maybe one word in "dictionary" has less length but more occurrence
+# e.g. s="wawao" , word1="wa" and word2="wao". word1 is more optimal
+
+
+# solution 3: sort dictionary by length of word
+   
 
 if __name__ == '__main__':
     unittest.main()
--- Math/2707_extra_strings_in_a_string/math_2707.py
+++ Math/2707_extra_strings_in_a_string/math_2707.py
@@ -1,4 +1,20 @@
+import pdb
+
 class Solution:
+    def minExtraChar(self, s: str, dictionary: list[str]) -> int:
+        # sort "dictionary" by length of its words, descending order
+        dictionary.sort(key=len, reverse=True)
+        for word in dictionary:
+            if word in s: # this if chunk is just for debugging
+                s = s.replace(word, "_"*len(word))
+                # breakpoint()
+                print(word)
+                print(s)
+        return len(s)
+
+
+
+
     def minExtraChar_dep2(self, s: str, dictionary: list[str]) -> int:
         res = len(s)
         for word in dictionary:
