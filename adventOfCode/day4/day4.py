4ab7f3c8a324cb06b79ca4beeec4ece1bfbd101b
HuachenZH <huachen.zhang.23@eigsi.fr>
2024-12-05 11:05:34 +0100

    building part2

--- adventOfCode/day4/day4.py
+++ adventOfCode/day4/day4.py
@@ -34,13 +34,36 @@ def part_1(path:str):
 
 
 
+def part_2(path:str):
+    with open(path, "r") as f:
+        data = f.read().strip()
+    res = 0
+
+    width = len(data.split("\n"))
+    #tmp = [  [ data.split("\n")[irow-1][icol-1] + data.split("\n")[irow-1][icol+1] + data.split("\n")[irow+1][icol-1] + data.split("\n")[irow+1][icol+1]  for icol in range(1, width-1) if data.split("\n")[irow][icol] == "A"  ] for irow in range(1, width-1)  ]
+    tmp = [item for sublist in [  [ data.split("\n")[irow-1][icol-1] + data.split("\n")[irow-1][icol+1] + data.split("\n")[irow+1][icol-1] + data.split("\n")[irow+1][icol+1]  for icol in range(1, width-1) if data.split("\n")[irow][icol] == "A"  ] for irow in range(1, width-1)  ] for item in sublist]
+
+    #tmp = [  [ data.split("\n")[irow-1][icol-1] + data.split("\n")[irow-1][icol+1] + data.split("\n")[irow+1][icol-1] + data.split("\n")[irow+1][icol+1]  for icol in range(1, width-1) if data.split("\n")[irow][icol] == "A"  ] for irow in range(1, width-1)  ]
+    #tmp = [item for item in sublist  for sublist in tmp ] # syntax error
+    #tmp = [item   for sublist in tmp for item in sublist] # correct
+
+    # to_resume:
+    # count correct terms in tmp
+    breakpoint()
+
+
+
+
 def main():
+    print("part 1:")
     print(part_1("data.txt"))
+    print("part 2:")
+    print(part_2("sample.txt"))
 
 
 
 def test():
-
+    # kudo to https://stackoverflow.com/questions/6313308/get-all-the-diagonals-in-a-matrix-list-of-lists-in-python
     matrix = np.array(
              [[-2,  5,  3,  2],
               [ 9, -6,  5,  1],
