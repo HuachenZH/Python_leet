63f570ae427e4fe9b7a12cae1a9529f94f99302b
HuachenZH <huachen.zhang.23@eigsi.fr>
2024-12-17 18:15:49 +0100

    day11 part1 passed

--- adventOfCode/day11/day11.py
+++ adventOfCode/day11/day11.py
@@ -1,8 +1,10 @@
 import copy
+from tqdm import tqdm
 
 
 
 def part_1_deprecated(path:str, blink:int):
+    """Result incorrect, root cause: indexing"""
     with open(path, "r") as f:
         data =  [int(num) for num in f.read().strip().split(" ")]
 
@@ -19,18 +21,52 @@ def part_1_deprecated(path:str, blink:int):
                 data = data[:i] + [int( str(v)[:int(len(str(v))/2)] )] + [int(str(v)[int(len(str(v))/2):])] + data[i+1:]
             else:
                 print("else")
-                data[i] = v * 1024
+                data[i] = v * 2024
             print(data)
 
-    breakpoint()
 
+
+def flatten_list(nested_list:list) -> list:
+    """Flatten a list containing integers and lists of integers."""
+    flat_list = []
+    for item in nested_list:
+        if isinstance(item, list):
+            flat_list.extend(flatten_list(item))  # Recursively flatten the sublist
+        else:
+            flat_list.append(item)  # Append the integer
+    return flat_list
+
+
+
+def part_1_flatten_list(path:str, blink:int):
+    with open(path, "r") as f:
+        data =  [int(num) for num in f.read().strip().split(" ")]
+        #__print(data)
+
+    for _ in tqdm(range(blink)):
+        for i,v in enumerate(tqdm(data, leave=False)):
+            if v==0:
+                data[i] = 1
+            elif len(str(v)) % 2 == 0:
+                data[i] = [int( str(v)[:int(len(str(v))/2)] ),   int(str(v)[int(len(str(v))/2):])]
+            else:
+                data[i] = v * 2024
+        # Flatten list before next blink
+        data = flatten_list(data)
+        #__print(data)
+    print(len(data))
+
+
+
+def part_1_linked_list(path:str, blink:int):
+    pass
 
 
 
 def main():
-    path = "test.txt"
-    blink = 2
-    part_1(path, blink)
+    path = "data.txt"
+    blink = 25
+    part_1_flatten_list(path, blink)
 
 
 
