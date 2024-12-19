e182dfc8c89d7645ee8e71d5ee721685f8b74b19
HuachenZH <huachen.zhang.23@eigsi.fr>
2024-12-19 22:01:59 +0100

    part1 finished...?

--- adventOfCode/day12/day12.py
+++ adventOfCode/day12/day12.py
@@ -27,37 +27,35 @@ def get_coords_of_flower(arr_data, flower) -> set[tuple[int,int]]:
 
 
 
-def construct_clusters_of_flower(coords:set) -> list[list[tuple]]:
+def construct_clusters_of_flower(coords:set) -> list[set[tuple]]:
     test_list = list(coords)
 
     # Group Adjacent Coordinates
     # Using product() + groupby() + list comprehension
     man_tups = [sorted(sub) for sub in product(test_list, repeat = 2)
                                             if Manhattan(*sub) == 1]
-    man_tups += man_tups
+    man_tups = man_tups * 10 # 1471452 improvement_1
     res_dict = {ele: {ele} for ele in test_list}
     for tup1, tup2 in man_tups:
-        print(tup1, tup2)
-        if tup1==(2,3) and tup2==(2,4):
-            #breakpoint()
-            pass
         res_dict[tup1] |= res_dict[tup2]
         res_dict[tup2] = res_dict[tup1]
-        if tup1==(2,3) and tup2==(2,4):
-            #breakpoint()
-            pass
 
-    res = [[*next(val)] for key, val in groupby(
+    res = [{*next(val)} for key, val in groupby(
             sorted(res_dict.values(), key = id), id)]
+    res2 = []
+    for elem in res:
+        if elem not in res2:
+            res2.append(elem)
+    
 
     # printing result 
     #print("The grouped elements : " + str(res)) 
-    return res
+    return res2
 
 
 
 
-def calculate_perimeter(one_cluster:list):
+def calculate_perimeter(one_cluster:set[tuple]):
     perimeter = 0
     for coord in one_cluster:
         neighbors = {(coord[0]+1, coord[1]),
@@ -77,17 +75,26 @@ def part_1(path:str) -> None:
         set_flowers = set(raw_data.replace("\n", ""))
 
     arr_data = np.array(data)
-    set_coords = get_coords_of_flower(arr_data, "R")
-    list_clusters = construct_clusters_of_flower(set_coords)
-    perim = calculate_perimeter(list_clusters[0])
-    breakpoint()
+    price = 0
+    for flower in set_flowers:
+        print(f"Flower: {flower}")
+        set_coords = get_coords_of_flower(arr_data, flower)
+        list_clusters = construct_clusters_of_flower(set_coords)
+        if flower == "A":
+            breakpoint()
+        for i,cluster in enumerate(list_clusters):
+            print(f"    cluster {i}:")
+            perim = calculate_perimeter(cluster)
+            price += perim * len(cluster)
+            print(f"    perim is {perim}")
+    print(price)
 
 
 
 def main():
-    path = "sample.txt"
+    path = "data.txt"
     part_1(path)
-    #debug_flower("sample.txt", "debug.txt", "R")
+    #debug_flower("data.txt", "debug.txt", "A")
 
 
 
@@ -95,3 +102,9 @@ if __name__ == "__main__":
     main()
 
 
+# Needs to improve
+# improvement_1 : see methods.md, to bypass the error, 
+# i multiplied man_tups by 10.
+# the true problem is at:
+#    res = [{*next(val)} for key, val in groupby(
+#            sorted(res_dict.values(), key = id), id)]
