db6fb8f35db636189e980177cdde896d80a60ae2
HuachenZH <huachen.zhang.23@eigsi.fr>
2025-03-10 23:16:18 +0100

    start part1

--- adventOfCode/day15/day15.py
+++ adventOfCode/day15/day15.py
@@ -1,25 +1,53 @@
 
-with open("data_warehouse.txt", "r") as f:
-    list_warehouse = f.read().split("\n")
-with open("data_movements.txt", "r") as f:
-    str_movements = "".join(f.read().split("\n"))
+with open("data_warehouse_small.txt", "r") as f:
+    WAREHOUSE_INIT = f.read().strip().split("\n") # list[str]
+with open("data_movements_small.txt", "r") as f:
+    MOVEMENTS = "".join(f.read().strip().split("\n")) # str
+
+
+def transpose_warehouse(list_warehouse:list) -> list[str]:
+    # if you run the list comprehension in pdb, it will say
+    # NameError: name 'list_warehouse' is not defined
+    # However if you run it in interactive session, it will be fine.
+    return [ "".join([row[icol] for row in list_warehouse]) 
+            for icol in range(len(list_warehouse))]
 
 
 
 
-def get_initial_position(arr_warehouse:list) -> tuple[int]:
+def get_initial_position(list_warehouse:list) -> tuple[int]:
+    # returns tuple[int], (irow, icol)
     for i,row in enumerate(list_warehouse):
         if row.find("@") != -1:
             return (i, row.find("@"))
 
 
 
+def look_ahead(list_warehouse:list, str_movement:str, 
+               curr_pos:tuple[int]) -> str:
+    if str_movement == "<":
+        return list_warehouse[curr_pos[0]][:curr_pos[1]]
+    if str_movement == ">":
+        return list_warehouse[curr_pos[0]][curr_pos[1]+1:]
+    if str_movement == "^":
+        list_warehouse_t = transpose_warehouse(list_warehouse)
+        return list_warehouse_t[curr_pos[1]][:curr_pos[0]]
+    if str_movement == "v":
+        list_warehouse_t = transpose_warehouse(list_warehouse)
+        return list_warehouse_t[curr_pos[1]][curr_pos[0]+1:]
+
+
+
 def part1():
     # get init pos
-    tup_init_pos = get_initial_position(list_warehouse)
-    breakpoint()
+    tup_init_pos = get_initial_position(WAREHOUSE_INIT)
 
     # look ahead
+    # test code
+    path_ahead = look_ahead(WAREHOUSE_INIT, "^", tup_init_pos)
+    breakpoint()
+
+    # check ahead
 
     # push box
     pass
@@ -31,3 +59,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
